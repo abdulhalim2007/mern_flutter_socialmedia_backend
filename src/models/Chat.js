e026e9563c6333078b1674648fbbc52888347ac9
unknown <abdulalisha2311@gmail.com>
2023-01-25 12:12:39 +0530

    modified

--- src/models/Chat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const ChatSchema = new mongoose.Schema({
-    user1: { type: mongoose.Types.ObjectId, ref: 'User' },
-    user2: { type: mongoose.Types.ObjectId, ref: 'User' },
-    chatName: [{ type: String }],
-}, { timestamps: true })
-
-module.exports = mongoose.model('Chat', ChatSchema)
