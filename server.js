8545a9887b18d5a9c15e4ccdac3ceaeebacb9261
abdulhalim2007 <87314918+abdulhalim2007@users.noreply.github.com>
2023-02-14 17:19:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();}
+    next();})
 app.use('/images', express.static(path.join(__dirname, "public/images")))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
